8da41cb1111174e7261865768ec99e7b99e2ddb5
weshays <weshays@gbdev.com>
2013-05-07 16:58:48 -0700

    Added CSRF token

--- javascripts/thecity_login.js
+++ javascripts/thecity_login.js
@@ -5,8 +5,8 @@ TheCityLogin = {
   defaults : { 'subdomain' : null,
                'city_login_div_id' : 'thecity_login'},
                
-  _class_vars : {'login_form_url' : 'http://authentication.onthecity.org/sessions/remote_form.json?callback=?',
-                 'login_url' : 'http://authentication.onthecity.org/sessions/remote_login.json'}, 
+  _class_vars : {'login_form_url' : 'http://authentication.devthecity.org:3002/sessions/remote_form.json?callback=?',
+                 'login_url' : 'http://authentication.devthecity.org:3002/sessions/remote_login.json'}, 
   
   start : function(options) {
     if(options !== undefined) { 
@@ -30,34 +30,25 @@ TheCityLogin = {
         form = form.replace(/[\r\n]/g, '');
         form = $.base64.decode(form);
         $('#'+self.defaults['city_login_div_id']).html(form);
+        
+        self._add_meta_tag_csrf_token();
+
         self._add_login_link_listener();
       }
     );    
   },
-  
+
+  _add_meta_tag_csrf_token : function() {
+    var csrf_token = document.createElement('meta');
+    csrf_token.name = "csrf-token";
+    csrf_token.content = $('[name="authenticity_token"]').val()
+    document.getElementsByTagName('head')[0].appendChild(csrf_token);
+  },
 
   _add_login_link_listener : function() {
-    var self = this; // Need object scope at this point
+    //var self = this; // Need object scope at this point
     $("#city_login_link").click(function() {
-      var params = {
-        'login' : $("#login").val(),
-        'password' : $("#password").val(),
-        'authenticity_token' : $('[name="authenticity_token"]').val()
-      }
-
-      $.post(self._class_vars['login_url'], params, function(response) {
-        console.log(data);
-        window.location = response['redirect_to_url'];
-      }).fail(function(jqXHR, textStatus, errorThrown) { 
-        // var errors_messages = $.parseJSON(jqXHR.responseText);
-        // alert(errors_messages['errors'][0]);
-        console.log("FAIL");
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
-        console.log("FAIL");
-      });
-
+      $("#city_login_link").parents('form').submit();
     });
   }
   
